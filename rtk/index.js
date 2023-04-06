d38d443c25eaad0b36711c288d4db05a3276d124
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2023-04-06 14:59:55 +0600

    middleware added in redux-toolkit

--- rtk/index.js
+++ rtk/index.js
@@ -2,19 +2,19 @@ const store = require("./app/store");
 const { counterActions } = require('./features/counter/counterSlice');
 const { dynamicCounterActions } = require('./features/dynamicCounter/dynamicCounterSlice');
 
-console.log(`Initial State ${JSON.stringify(store.getState())}`)
+// console.log(`Initial State ${JSON.stringify(store.getState())}`)
 
 // subscribe to state changes
 store.subscribe(() => {
-    console.log(store.getState());
+    // console.log(store.getState());
 });
 
 // dispatch actions
-// store.dispatch(counterActions.increment());
+store.dispatch(counterActions.increment());
 
-// store.dispatch(counterActions.increment());
+store.dispatch(counterActions.increment());
 
-// store.dispatch(counterActions.decrement());
+store.dispatch(counterActions.decrement());
 
 // dynamic counter actions
 
